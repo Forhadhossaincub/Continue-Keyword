f8be3c3438441fe1042fb9af9fad9f2ee9390fb1
NET <NET@DESKTOP-NSMUMFG>
2022-10-16 15:51:54 +0600

    Add project files.

---
+++ Continue Keyword/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i == 10) continue;
+
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
+
